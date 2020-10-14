3fe02af663d191fa7726c2389d8851e459f71bc9
eleo huayhua <ehuayhuap@unsa.edu.pe>
2020-10-13 20:57:48 -0500

    Creacion del nivel 1

---
+++ PAME-Games/Assets/Script/cambiarEscena.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cambiarEscena : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+     public void CambiarEscena(string nombre){
+        print("cambiaste a la escena"+nombre);
+        SceneManager.LoadScene(nombre);
+    }
+}
---
+++ PAME-Games/Assets/camaraSeguir.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camaraSeguir : MonoBehaviour
+{
+    public GameObject follow;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float posX = follow.transform.position.x;
+        float posY = follow.transform.position.y;
+
+        transform.position = new Vector3(posX,posY,transform.position.z);
+    }
+}
