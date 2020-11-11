c58c724a2a0d25e752757848e455714ff7d45774
EALaura <elauraba@unsa.edu.pe>
2020-11-10 19:37:35 -0500

    Se agrega puntajes a las monedas

---
+++ PAME-Games/Assets/Script/GameControll.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControll : MonoBehaviour
+{
+    public static int Coin = 0;
+    public string CoinString = "Monedas: ";
+
+    public Text TextCoin;
+    public static GameControll Gamecontroll;
+
+    void Awake()
+    {
+        Gamecontroll = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TextCoin != null)
+        {
+            TextCoin.text = CoinString + "" + (Coin);
+        }
+    }
+}
