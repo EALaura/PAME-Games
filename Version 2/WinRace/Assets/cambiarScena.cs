58130d6d8ff7d75b73eeb11cfbe06cca166e0ab2
eleo huayhua <ehuayhuap@unsa.edu.pe>
2020-12-22 23:34:35 -0500

    pantalla principal

---
+++ Version 2/WinRace/Assets/cambiarScena.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cambiarScena : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ingresaNombre(string text){
+        SceneManager.LoadScene(text);
+    }
+    public void Salir(){
+        Application.Quit();
+    }
+}
